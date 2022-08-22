10d4e2d109a286b81f254e6471a9b23295d8e975
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-08-22 11:26:19 +0600

    added validation for checking the input value is number or not

--- js/script.js
+++ js/script.js
@@ -3,13 +3,12 @@ const playersDisplayContainer = document.getElementById('players-display-contain
 // select player and display all selected players
 function selectPlayer(name, button) {
     const playerName = getElementTexetById(name, false);
-
     const p = document.createElement('li');
 
     // players can not selected more than five
     const playersCount = playersDisplayContainer.childElementCount;
     if (playersCount > 4) {
-        alert("Players Limit Exceeded!!");
+        alert("Players Limit Exceeded!!!");
         return;
     }
     else {
@@ -20,15 +19,20 @@ function selectPlayer(name, button) {
     // after selected button will be disabled
     const selectButton = document.getElementById(button);
     selectButton.setAttribute('disabled', true);
-    selectButton.style.backgroundColor = 'grey';
     selectButton.style.border = '0';
 }
 
 // total players price calculation
 document.getElementById('btn-calculate').addEventListener('click', function () {
     const totalPlayers = playersDisplayContainer.childElementCount;
-
     const perPlayerExpense = getInputValueById('per-player-input-field');
+
+    // validation for checking the input value is number or not
+    if (isNaN(perPlayerExpense)) {
+        alert('Please give a number in per player input!');
+        return;
+    }
+
     const playersExpenses = perPlayerExpense * totalPlayers;
     setElementextById('player-expenses-element', playersExpenses);
 })
@@ -38,6 +42,12 @@ document.getElementById('btn-calculate-total').addEventListener('click', functio
     const playersExpenses = getElementTexetById('player-expenses-element', true);
     const managerCost = getInputValueById('manager-input-field');
     const coachCost = getInputValueById('coach-input-field');
+
+    // validation for checking the input value is number or not
+    if (isNaN(playersExpenses) || isNaN(managerCost) || isNaN(coachCost)) {
+        alert('Please give a number input for final calculation!');
+        return;
+    }
 
     const totalCost = playersExpenses + managerCost + coachCost;
     setElementextById('total-element', totalCost);
