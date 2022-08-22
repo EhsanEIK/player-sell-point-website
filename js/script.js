19d265d3693645296e8ef494ed0c2abc462ad299
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-08-22 10:15:28 +0600

    added condition: players can not added more than five

--- js/script.js
+++ js/script.js
@@ -1,12 +1,23 @@
+// select player and display all selected players
 function selectPlayer(name, button) {
     const nameElementOfPlayer = document.getElementById(name);
     const playerName = nameElementOfPlayer.innerText;
 
     const playersDisplayContainer = document.getElementById('players-display-container');
     const p = document.createElement('li');
-    p.innerText = playerName;
-    playersDisplayContainer.appendChild(p);
 
+    // players can not selected more than five
+    const playersCount = playersDisplayContainer.childElementCount;
+    if (playersCount > 4) {
+        alert("Players Limit Exceeded!!");
+        return;
+    }
+    else {
+        p.innerText = playerName;
+        playersDisplayContainer.appendChild(p);
+    }
+
+    // after selected button will be disabled
     const selectButton = document.getElementById(button);
     selectButton.setAttribute('disabled', true);
     selectButton.style.backgroundColor = 'grey';
